98b0b27aeb6d7075caecbe60ca61cd54f36f7ed8
Shura <SchadowofMorning@web.de>
2017-07-27 23:12:46 +0200

    0.0.4
    First build try

--- Saver.js
+++ Saver.js
@@ -1,15 +1,10 @@
 var saver = module.exports;
 const fs = require('fs')
 const request = require('request')
-var index = require('./index.json')
+const DataStore = require('nedb')
+
 saver.SaveLink = function(url, name){
     var page = request(url)
-    var pageobj = {"Name":"" , "URL":""}
     var file = fs.createWriteStream(__dirname + "/savedpages/" + name + ".html")
-    pageobj.Name = name;
-    pageobj.URL = url;
-    fs.writeFile( "./index.json", JSON.stringify(pageobj), function(err){
-      if(err) throw err;
-    })
     page.pipe(file)
 }
--- main.js
+++ main.js
@@ -3,35 +3,83 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-const {ipcMain} = require('electron')
+const {ipcMain, webContents} = require('electron')
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
 const http = require('http')
 const saver = require('./Saver.js')
+var DataStore = require('nedb')
+const request = require('request')
+const folder = "./"
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+//Data Storage
+fs.readdirSync(folder, (err, files) =>{
 
+})
+var index = new DataStore({filename: folder + "/index", autoload: true});
 
 //Logic
-ipcMain.on('search', (event, arg) =>{
-  console.log(arg);
-  saver.SaveLink(arg.uri, arg.name)
+ipcMain.on('save', (event, page) =>{
+  saver.SaveLink(page.uri, page.name)
+  index.insert({Name: page.name, URL: page.uri, Changes: false, type:"website"})
+  event.sender.send('page', {"Name": page.name, "URL": page.uri, Changes: false})
 })
-ipcMain.on('init', (event, arg) => {
-  fs.readdir('./savedpages', (err, files) => {
-    files.forEach(file => {
-      event.sender.send('page', file)
-    })
+ipcMain.on('compare', (event, arg) => {
+  index.find({"type":"website"}, function(err, docs){
+    for(i = 0; i < docs.length; i++){
+      console.log(docs[i]);
+      var filename = docs[i].Name;
+      var url = docs[i].URL;
+      var ID = docs[i]._id;
+      var data = fs.readFileSync(__dirname + "/savedpages/" + filename + ".html");
+        console.log(data);
+        var page = request(url, function(err, resp, body){
+          console.log(body);
+          if(data == body){
+            index.update({_id: ID}, {$set: {Changes:false}}, function(err, updateval){
+              if(err) throw err;
+              event.sender.send('update-false', filename + "-changescell")
+            })
+          } else {
+            index.update({_id: ID}, {$set: {Changes:true}}, function(err, updateval){
+              if(err) throw err;
+              event.sender.send('update-true', filename + "-changescell")
+            })
+          }
+        })
+
+    }
   })
 })
+ipcMain.on('init', (event, arg) => {
 
+  index.findOne({type:"UserSettings"}, function(err, doc){
+      if(doc == null){
+        index.insert({type:"UserSettings", DataPath:"./"})
+      } else {
+        event.sender.send('SetupUser', doc)
+      }
+  })
+  index.find({type:"website"}, function(err, docs){
+    if(err) throw err;
+    console.log(docs)
+    for(i = 0; i < docs.length; i++){
+      event.sender.send('page', docs[i]);
+    }
+  })
+
+
+
+
+})
 //End Logic
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon:"./Webwatcher.ico"})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -39,6 +87,7 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }))
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
